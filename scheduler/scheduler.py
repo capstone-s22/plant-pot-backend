2e35d2f984ad0d814c5ec2374ade2c266421cac6
Lee Hang Wee <leehangwee@gmail.com>
2021-06-22 02:00:31 +0800

    checkin at 1200 SGT

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -8,7 +8,7 @@ async def daily_check_in_alert():
     await manager.broadcast("CheckIn")
 
 scheduler = AsyncIOScheduler({'apscheduler.timezone': 'UTC'})
-# scheduler.add_job(broadcast, 'interval', seconds=5)
 
-scheduler.add_job(broadcast, 'cron', hour=17, minute=55)
+# UTC Time is 8 hours ahead of SGT, so UTC 1600 == SGT 0000
+scheduler.add_job(broadcast, 'cron', hour=16)
 scheduler.start()
