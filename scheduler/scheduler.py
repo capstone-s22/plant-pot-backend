92275f3b6c66c46c0a3de45f6951d7fce2077778
Lee Hang Wee <leehangwee@gmail.com>
2021-07-19 00:35:26 +0800

    continue sending to other pots if 1 pot fails

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -24,7 +24,7 @@ async def pots_health_check():
             firestore_input = {"connected": True}
 
         except Exception as e:
-            logger.error("Health check to pot {} failed!".format(pot_id))
+            logger.error("Health check to Pot {} failed!".format(pot_id))
             firestore_input = {"connected": False}
             
         # Update Firebase to alert mobile app
@@ -34,19 +34,22 @@ async def daily_check_in_alert():
     all_pots = pots_collection.get()
 
     for pot in all_pots:
-        pot_id = pot.to_dict()['potId']
-        firestore_input = {"session.checkIn.showCheckIn": True}
-        # Update Firebase to alert mobile app
-        pots_collection.document(pot_id).update(firestore_input)
-        # Alert Pot
-        alert_message = MessageToPot(action=Action.update,
-                                potId=pot_id, 
-                                data=[PotSendDataDictBool(
-                                    field=PotSendDataBool.showCheckIn,
-                                    value=True)])
-        await ws_manager.send_personal_message_json(alert_message.dict(), pot_id)
-        logger.info("Sent Check In alert to Pot {}".format(pot_id))
-        # TODO: Need a message queue for messages not sent to pots with failed websocket connection
+        try:
+            pot_id = pot.to_dict()['potId']
+            firestore_input = {"session.checkIn.showCheckIn": True}
+            # Update Firebase to alert mobile app
+            pots_collection.document(pot_id).update(firestore_input)
+            # Alert Pot
+            alert_message = MessageToPot(action=Action.update,
+                                    potId=pot_id, 
+                                    data=[PotSendDataDictBool(
+                                        field=PotSendDataBool.showCheckIn,
+                                        value=True)])
+            await ws_manager.send_personal_message_json(alert_message.dict(), pot_id)
+            logger.info("Sent Check In alert to Pot {}".format(pot_id))
+            # TODO: Need a message queue for messages not sent to pots with failed websocket connection
+        except Exception as e:
+            logger.error("Check In alert to Pot {} failed!".format(pot_id))
 
 async def quiz_alert():
     current_date = datetime.utcnow().strftime('%Y%m%d')
@@ -55,32 +58,36 @@ async def quiz_alert():
 
     # TODO: parse this properly to Pot object
     for pot in retrieved_pots:
-        pot_id = pot.to_dict()["potId"]
-        quiz_day_number_idx = pot.to_dict()['session']['quiz']['quizDates'].index(current_date)
-        quiz_day_number = pot.to_dict()['session']['quiz']['quizDayNumbers'][quiz_day_number_idx]
-        current_show_quiz_numbers: list = pot.to_dict()['session']['quiz']['showQuizNumbers']
-        firestore_input = {"session.quiz.showQuizNumbers": current_show_quiz_numbers.append(quiz_day_number),
-                            "session.quiz.currentQuizDayNumber" : quiz_day_number}
+        try:
+            pot_id = pot.to_dict()["potId"]
+            quiz_day_number_idx = pot.to_dict()['session']['quiz']['quizDates'].index(current_date)
+            quiz_day_number = pot.to_dict()['session']['quiz']['quizDayNumbers'][quiz_day_number_idx]
+            current_show_quiz_numbers: list = pot.to_dict()['session']['quiz']['showQuizNumbers']
+            firestore_input = {"session.quiz.showQuizNumbers": current_show_quiz_numbers.append(quiz_day_number),
+                                "session.quiz.currentQuizDayNumber" : quiz_day_number}
 
-        # Update Firebase to alert mobile app
-        pots_collection.document(pot_id).update(firestore_input)
-        logger.info("Updated Quiz {} alert for Pot {} to database".format(quiz_day_number, pot_id))
-        
-        #TODO: Also alert when previous quiz not yet completed 
-        # Alert Pot
-        alert_message = MessageToPot(action=Action.update,
-                                potId=pot_id, 
-                                data=[PotSendDataDictBool(
-                                    field=PotSendDataBool.showQuiz,
-                                    value=True)])
-        await ws_manager.send_personal_message_json(alert_message.dict(), pot_id)
+            # Update Firebase to alert mobile app
+            pots_collection.document(pot_id).update(firestore_input)
+            logger.info("Updated Quiz {} alert for Pot {} to database".format(quiz_day_number, pot_id))
+            
+            #TODO: Also alert when previous quiz not yet completed 
+            # Alert Pot
+            alert_message = MessageToPot(action=Action.update,
+                                    potId=pot_id, 
+                                    data=[PotSendDataDictBool(
+                                        field=PotSendDataBool.showQuiz,
+                                        value=True)])
+            await ws_manager.send_personal_message_json(alert_message.dict(), pot_id)
 
-        logger.info("Sent Quiz {} alert to Pot {}".format(quiz_day_number, pot_id))
+            logger.info("Sent Quiz {} alert to Pot {}".format(quiz_day_number, pot_id))
+
+        except Exception as e:
+            logger.error("Quiz {} alert to Pot {} failed!".format(quiz_day_number, pot_id))
 
 scheduler = AsyncIOScheduler({'apscheduler.timezone': 'UTC'})
 
 scheduler.add_job(pots_health_check, 'interval', hours=1)
 # UTC Time is 8 hours ahead of SGT, so UTC 1600 == SGT 0000
-scheduler.add_job(daily_check_in_alert, 'cron', hour=16)
-scheduler.add_job(quiz_alert, 'cron', hour=16, minute=30)
+scheduler.add_job(daily_check_in_alert, 'cron', hour=16, minute=37)
+scheduler.add_job(quiz_alert, 'cron', hour=16, minute=37)
 scheduler.start()
