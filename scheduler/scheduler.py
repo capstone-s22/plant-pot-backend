65062e207e5d7729fb96c8fd5ba4e6b34138c567
Lee Hang Wee <leehangwee@gmail.com>
2021-07-19 21:07:22 +0800

    test checkin

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -90,6 +90,6 @@ scheduler = AsyncIOScheduler({'apscheduler.timezone': 'UTC'})
 scheduler.add_job(pots_health_check, 'interval', hours=1)
 # UTC Time is 8 hours ahead of SGT, so UTC 1600 == SGT 0000
 # NOTE: Somehow AsyncIOScheduler can't run concurrently, TODO: research on this
-scheduler.add_job(daily_check_in_alert, 'cron', hour=13, minute=6)
+scheduler.add_job(daily_check_in_alert, 'cron', hour=13, minute=9)
 scheduler.add_job(quiz_alert, 'cron', hour=16, minute=1)
 scheduler.start()
