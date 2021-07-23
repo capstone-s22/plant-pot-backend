501aba99f0f0880929a782afdf4c92ec0f9b647a
Lee Hang Wee <leehangwee@gmail.com>
2021-07-23 22:16:56 +0800

    clear redundant code

--- lib/errors.py
+++ lib/errors.py
@@ -1,14 +1,5 @@
-import json
-
 class PotNotFound(Exception):
     def __init__(self, message):
         self.message = message
     def __str__(self):
         return self.message
-
-test = {'a': 1, "b": 2}
-
-try:
-    raise PotNotFound(json.dumps(test))
-except Exception as e:
-    print(e)
